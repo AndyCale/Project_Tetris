ac0bb310ee743ca23225c2caae672c4fb8b60ad6
Andy_Cale <aibykovs@mail.ru>
2020-01-04 21:09:44 +0700

    Beginning menu commit

--- menu.py
+++ menu.py
@@ -1,15 +1,12 @@
 import pygame
 import os
 import sys
+from random import choice
 
 
 pygame.init()
 size = width, height = 550, 550
 screen = pygame.display.set_mode(size)
-
-FPS = 50
-clock = pygame.time.Clock()
-running = True
 
 
 def load_image(name, colorkey=None):
@@ -49,9 +46,42 @@ def start_screen():
             if event.type == pygame.QUIT:
                 terminate()
             elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
-                return  # начинаем игру
+                return
         pygame.display.flip()
         clock.tick(FPS)
 
-start_screen()
+
+def menu():
+    screen.fill((0, 0, 0))
+    font = pygame.font.Font(None, 50)
+    for let in range(len("TeTris")):
+        text = font.render("TeTris"[let], 1, (choice(range(50, 255)), choice(range(50, 255)), choice(range(50, 255))))
+        text_x = 145 + let * 50
+        text_y = 50
+        screen.blit(text, (text_x, text_y))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                terminate()
+            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                terminate()
+                pos = event.pos
+                game()
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
+def game():
+    pass
+
+
+FPS = 50
+clock = pygame.time.Clock()
+running = True
+menu()
+
+
 pygame.quit()
