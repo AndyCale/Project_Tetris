79ac6ba85619e53c222d51ebe0872f5c988a93a4
Andy_Cale <aibykovs@mail.ru>
2020-01-04 22:53:06 +0700

    Colors button

--- menu.py
+++ menu.py
@@ -61,6 +61,8 @@ def menu():
         text_y = 60
         screen.blit(text, (text_x, text_y))
 
+    color = []
+
     for word in range(len(["Играть", "Рекорды", "Об игре", "Выйти"])):
         font = pygame.font.Font(None, 40)
         text = font.render(["Играть", "Рекорды", "Об игре", "Выйти"][word], 1, (0, 0, 0))
@@ -68,10 +70,12 @@ def menu():
         text_y = 195 + word * 65
         text_w = text.get_width()
         text_h = text.get_height()
-        pygame.draw.rect(screen, (choice(range(50, 255)), choice(range(50, 255)), choice(range(50, 255))),
+        color.append((choice(range(50, 255)), choice(range(50, 255)), choice(range(50, 255))))
+        pygame.draw.rect(screen, color[-1],
                          (width // 2 - 95, text_y - 10, 190, text_h + 20))
         pygame.draw.rect(screen, (255, 255, 255), (width // 2 - 95, text_y - 10, 190, text_h + 20), 1)
         screen.blit(text, (text_x, text_y))
+        print(width // 2 - 95, text_y - 10, 190, text_h + 20, text_x, text_y)
 
     # Имя игрока - Здрасте игрок
 
@@ -86,19 +90,74 @@ def menu():
                 if 180 <= pos[0] <= 370 and 185 <= pos[1] <= 233:
                     game()
                     print(1)
-                if 180 <= pos[0] <= 370 and 250 <= pos[1] <= 298:
+                elif 180 <= pos[0] <= 370 and 250 <= pos[1] <= 298:
                     print("Рекорды")
-                if 180 <= pos[0] <= 370 and 315 <= pos[1] <= 363:
+                elif 180 <= pos[0] <= 370 and 315 <= pos[1] <= 363:
                     print("Об игре")
-                if 180 <= pos[0] <= 370 and 380 <= pos[1] <= 428:
+                elif 180 <= pos[0] <= 370 and 380 <= pos[1] <= 428:
                     print(2)
                     terminate()
-
-
-
             elif pygame.mouse.get_focused():
+
                 pos = pygame.mouse.get_pos()
-                #if pos[0]
+                if 180 <= pos[0] <= 370 and 185 <= pos[1] <= 233:
+                    font = pygame.font.Font(None, 40)
+                    text = font.render("Играть", 1, (0, 0, 0))
+                    pygame.draw.rect(screen, (color[0][0] + 30 if color[0][0] + 30 <= 255 else 255,
+                                              color[0][1] + 30 if color[0][1] + 30 <= 255 else 255,
+                                              color[0][2] + 30 if color[0][2] + 30 <= 255 else 255),
+                                     (180, 185, 190, 48))
+                    pygame.draw.rect(screen, (255, 255, 255), (180, 185, 190, 48), 1)
+                    screen.blit(text, (229, 195))
+                elif 180 <= pos[0] <= 370 and 250 <= pos[1] <= 298:
+                    font = pygame.font.Font(None, 40)
+                    text = font.render("Рекорды", 1, (0, 0, 0))
+                    pygame.draw.rect(screen, (color[1][0] + 30 if color[1][0] + 30 <= 255 else 255,
+                                              color[1][1] + 30 if color[1][1] + 30 <= 255 else 255,
+                                              color[1][2] + 30 if color[1][2] + 30 <= 255 else 255),
+                                     (180, 250, 190, 48))
+                    pygame.draw.rect(screen, (255, 255, 255), (180, 250, 190, 48), 1)
+                    screen.blit(text, (212, 260))
+                elif 180 <= pos[0] <= 370 and 315 <= pos[1] <= 363:
+                    font = pygame.font.Font(None, 40)
+                    text = font.render("Об игре", 1, (0, 0, 0))
+                    pygame.draw.rect(screen, (color[2][0] + 30 if color[2][0] + 30 <= 255 else 255,
+                                              color[2][1] + 30 if color[2][1] + 30 <= 255 else 255,
+                                              color[2][2] + 30 if color[2][2] + 30 <= 255 else 255),
+                                     (180, 315, 190, 48))
+                    pygame.draw.rect(screen, (255, 255, 255), (180, 315, 190, 48), 1)
+                    screen.blit(text, (222, 325))
+                elif 180 <= pos[0] <= 370 and 380 <= pos[1] <= 428:
+                    font = pygame.font.Font(None, 40)
+                    text = font.render("Выйти", 1, (0, 0, 0))
+                    pygame.draw.rect(screen, (color[3][0] + 30 if color[3][0] + 30 <= 255 else 255,
+                                              color[3][1] + 30 if color[3][1] + 30 <= 255 else 255,
+                                              color[3][2] + 30 if color[3][2] + 30 <= 255 else 255),
+                                     (180, 380, 190, 48))
+                    pygame.draw.rect(screen, (255, 255, 255), (180, 380, 190, 48), 1)
+                    screen.blit(text, (230, 390))
+                else:
+                    font = pygame.font.Font(None, 40)
+                    text = font.render("Играть", 1, (0, 0, 0))
+                    pygame.draw.rect(screen, color[0], (180, 185, 190, 48))
+                    pygame.draw.rect(screen, (255, 255, 255), (180, 185, 190, 48), 1)
+                    screen.blit(text, (229, 195))
+                    font = pygame.font.Font(None, 40)
+                    text = font.render("Рекорды", 1, (0, 0, 0))
+                    pygame.draw.rect(screen, color[1], (180, 250, 190, 48))
+                    pygame.draw.rect(screen, (255, 255, 255), (180, 250, 190, 48), 1)
+                    screen.blit(text, (212, 260))
+                    font = pygame.font.Font(None, 40)
+                    text = font.render("Об игре", 1, (0, 0, 0))
+                    pygame.draw.rect(screen, color[2], (180, 315, 190, 48))
+                    pygame.draw.rect(screen, (255, 255, 255), (180, 315, 190, 48), 1)
+                    screen.blit(text, (222, 325))
+                    font = pygame.font.Font(None, 40)
+                    text = font.render("Выйти", 1, (0, 0, 0))
+                    pygame.draw.rect(screen, color[3], (180, 380, 190, 48))
+                    pygame.draw.rect(screen, (255, 255, 255), (180, 380, 190, 48), 1)
+                    screen.blit(text, (230, 390))
+
         pygame.display.flip()
         clock.tick(FPS)
 
